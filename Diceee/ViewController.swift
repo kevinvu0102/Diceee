9c8fad893c4f34b6d6fe02ab0c3e8948f352d06d
kevinvu0102 <kevinvu0102@yahoo.com>
2017-09-14 16:28:46 -0700

    Dicee App
    Dicee

---
+++ Diceee/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Diceee
+//
+//  Created by PoGo on 9/4/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        updateDiceImages()
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateDiceImages()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateDiceImages(){
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+        //print(randomDiceIndex1 + 1 + randomDiceIndex2 + 1 )
+
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+        
+    }
+
+}
+
